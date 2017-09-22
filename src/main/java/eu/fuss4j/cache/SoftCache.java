e041237b2f2086ad34071d85b8df2f794082a29a
Octavian Nita <octavian.nita@gmail.com>
2017-09-22 17:31:53 +0200

    implementing basic memoization

--- src/main/java/eu/fuss4j/cache/SoftCache.java
+++ src/main/java/eu/fuss4j/cache/SoftCache.java
@@ -8,10 +8,11 @@ import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Function;
 
 /**
- * A poor man's {@link SoftReference}-based, LRU and concurrent by default, cache.
+ * A poor man's bounded cache.
  *
  * @author Octavian Theodor NITA (https://github.com/octavian-nita/)
  * @version 1.0, Aug 16, 2017
+ * @implSpec The default implementation is a {@link SoftReference}-based, LRU and concurrent.
  * @see <a href="https://www.ibm.com/developerworks/library/j-jtp01246/index.html">Plugging memory leaks with soft
  * references</a>
  */
@@ -23,7 +24,10 @@ public class SoftCache<K, V> {
 
     protected final Lock lock;
 
-    public SoftCache() { this(16384); }
+    /**
+     * @implSpec Equivalent to calling <code>new SoftCache(4096)</code>.
+     */
+    public SoftCache() { this(4096); }
 
     public SoftCache(int maxSize) {
         if (maxSize <= 0) {
@@ -52,6 +56,10 @@ public class SoftCache<K, V> {
         }
     }
 
+    /**
+     * @return the backing {@link Map structure} that holds actual data
+     * @implNote The default method implementation is not thread-safe / synchronized.
+     */
     protected Map<K, V> cache() {
         Map<K, V> cache = cacheRef == null ? null : cacheRef.get();
         if (cache == null) {
@@ -60,8 +68,12 @@ public class SoftCache<K, V> {
         return cache;
     }
 
+    /**
+     * @param maxSize the maximum number of elements the cache is able to store
+     * @return a new instance of the backing {@link Map structure} to hold actual data
+     */
     protected Map<K, V> createCache(int maxSize) {
-        return new LinkedHashMap<K, V>(maxSize, 0.75f, true /* LRU... */) {
+        return new LinkedHashMap<K, V>(16, 0.75f, true /* LRU... */) {
 
             @Override
             protected boolean removeEldestEntry(Map.Entry<K, V> eldest) { return size() > maxSize; }
